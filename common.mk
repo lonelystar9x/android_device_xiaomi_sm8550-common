9d31a6ee0bd170dbfa4981663950bc092a81d632
basamaryan <basam.aryan@gmail.com>
2023-11-04 20:19:56 +0100

    sm8550-common: Use the common excluded-input-devices.xml
    Change-Id: I22da0119acb1df87eb093ab311fa86369817ca8a

--- common.mk
+++ common.mk
@@ -415,6 +415,9 @@ $(call soong_config_set,qti_vibrator,use_effect_stream,true)
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
 
+PRODUCT_COPY_FILES += \
+    vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
