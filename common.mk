3928e2d07420e1c333db4e4e0545505217102356
Arian <arian.kulmer@web.de>
2025-02-18 18:00:35 +0100

    sm8550-common: Build qcrilNr.db from source
    Change-Id: I1f3425713b9b5b1cc7185bc94124b9f01efe3abb

--- common.mk
+++ common.mk
@@ -343,7 +343,8 @@ PRODUCT_PACKAGES += \
     qti-telephony-utils \
     qti_telephony_hidl_wrapper.xml \
     qti_telephony_hidl_wrapper_prd.xml \
-    qti_telephony_utils.xml
+    qti_telephony_utils.xml \
+    qcrilNrDb_vendor
 
 PRODUCT_PACKAGES += \
     xiaomi-telephony-stub
