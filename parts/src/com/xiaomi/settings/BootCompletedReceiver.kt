1fdc9a3a9e78299b66274d73fa68cbe2d281e429
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2025-04-17 10:45:22 +0200

    sm8550-common: Allow to enable/disable ESIM (eUICC)
    Change-Id: I4ad731dbfa94755e32f8627dc4f373dc3316fbf5

---
+++ parts/src/com/xiaomi/settings/BootCompletedReceiver.kt
@@ -0,0 +1,47 @@
+/*
+ * SPDX-FileCopyrightText: 2023-2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.xiaomi.settings
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.hardware.display.DisplayManager
+import android.os.UserHandle
+import android.util.Log
+import android.view.Display
+import android.view.Display.HdrCapabilities
+import com.xiaomi.settings.telephony.EsimController
+
+class BootCompletedReceiver : BroadcastReceiver() {
+
+    companion object {
+        private const val TAG = "BootReceiver"
+        private val DEBUG = Log.isLoggable(TAG, Log.DEBUG)
+    }
+
+    override fun onReceive(context: Context, intent: Intent) {
+        if (DEBUG) Log.d(TAG, "Received boot completed intent: ${intent.action}")
+        when (intent.action) {
+            Intent.ACTION_BOOT_COMPLETED -> onBootCompleted(context)
+        }
+    }
+
+    private fun onBootCompleted(context: Context) {
+        // Telephony
+        EsimController.getInstance(context).onBootCompleted()
+    }
+
+    private fun onLockedBootCompleted(context: Context) {
+        // Override HDR types to enable Dolby Vision
+        val displayManager = context.getSystemService(DisplayManager::class.java)
+        displayManager?.overrideHdrTypes(Display.DEFAULT_DISPLAY, intArrayOf(
+            HdrCapabilities.HDR_TYPE_DOLBY_VISION,
+            HdrCapabilities.HDR_TYPE_HDR10,
+            HdrCapabilities.HDR_TYPE_HLG,
+            HdrCapabilities.HDR_TYPE_HDR10_PLUS
+        ))
+    }
+}
