7bd64dddf3bd0fd4961beadf8edb4d3210eb4905
TheMysticle <36317711+TheMysticle@users.noreply.github.com>
2025-04-30 01:00:57 +0200

    sm8550-common: Add private SEPolicy files for charging, audio, camera, and settings
    Add private SEPolicy files for secure property and file contexts specific to MIUI
    and vendor HAL requirements.
    
    Changes include:
    - Introduced file_contexts for MIUI offline charging mode (`/system/bin/chargeonlymode`)
    - Added property_contexts for various system properties related to audio, camera, fingerprint,
      hardware, MIUI settings, and performance
    - Updated `hal_audio.te` in `sepolicy/vendor` to grant `hal_audio_default` access to sysfs files
      and directories

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -174,6 +174,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 
 # SEPolicy
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
 # System properties
