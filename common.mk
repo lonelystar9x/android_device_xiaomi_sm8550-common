6f0c2f740807b9a9df9d7d63a18ce980d92c18af
TheMysticle <36317711+TheMysticle@users.noreply.github.com>
2025-04-30 04:17:20 +0200

    sm8550-common: Build XiaomiParts
    Added XiaomiParts to PRODUCT_PACKAGES

--- common.mk
+++ common.mk
@@ -97,6 +97,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.automotive.vehicle@2.0-manager-lib
 
+#XiaomiParts
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # Atrace
 PRODUCT_PACKAGES += \
     android.hardware.atrace@1.0-service
