3b1296323f7ab0e66d9a8c779fb612866172b9ab
Arian <arian.kulmer@web.de>
2025-07-11 16:04:09 +0200

    sm8550-common: Don't explicitly build libwfdaac_vendor
    This has been converted to blueprint and is built implicitly as
    dependency of libwfdmmsrc_proprietary.so.
    
    Change-Id: I82bcca5f9aa85793c7b5a366e35472997e57feaf

--- common.mk
+++ common.mk
@@ -430,7 +430,3 @@ PRODUCT_PACKAGES += \
     firmware_wlanmdsp.otaupdate_symlink \
     firmware_wlan_mac.bin_symlink \
     firmware_WCNSS_qcom_cfg.ini_symlink
-
-# WiFi Display
-PRODUCT_PACKAGES += \
-    libwfdaac_vendor
