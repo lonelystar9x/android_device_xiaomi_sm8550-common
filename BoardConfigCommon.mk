8d8f5229a7597d66c6f06a6782793bf670cd3538
WenHao2130 <wenhao2130@outlook.com>
2025-09-26 09:46:00 +0800

    sm8550-common: Update from OS2.0.209.VMKCNXM
    Change-Id: I129044743e2c3fa53dad138473715d1f72d5bf6e
    Signed-off-by: WenHao2130 <wenhao2130@outlook.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -202,7 +202,7 @@ DEVICE_MANIFEST_FILE := \
     $(COMMON_PATH)/configs/vintf/manifest_socrates.xml
 
 # Vendor security patch
-VENDOR_SECURITY_PATCH := 2025-06-01
+VENDOR_SECURITY_PATCH := 2025-09-01
 
 # DeviceAsWebcam
 TARGET_BUILD_DEVICE_AS_WEBCAM := true
