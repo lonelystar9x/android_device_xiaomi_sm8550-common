8b39f3eedc8acd2273c5f3b4038ce44c76abc180
Lunark :3 <jvillaltamatos3007@gmail.com>
2025-10-23 10:56:33 -0500

    sm8550-common: Update from OS2.0.210.VMKCNXM
    Change-Id: I92f1c7717cc03b56b1c91a10d33b55b7d0c45cbd
    Signed-off-by: Lunark :3 <jvillaltamatos3007@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -199,7 +199,7 @@ DEVICE_MANIFEST_FILE := \
     $(COMMON_PATH)/configs/vintf/manifest_socrates.xml
 
 # Vendor security patch
-VENDOR_SECURITY_PATCH := 2025-09-01
+VENDOR_SECURITY_PATCH := 2025-10-01
 
 # DeviceAsWebcam
 TARGET_BUILD_DEVICE_AS_WEBCAM := true
