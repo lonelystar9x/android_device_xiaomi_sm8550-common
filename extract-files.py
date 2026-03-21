7c7d758c8ae5c1bc7761801d37ad3461605bc226
Lunark :3 <jvillaltamatos3007@gmail.com>
2026-03-20 19:58:32 -0500

    sm8550-common: init: Checkout init kalama specific scripts to LA.VENDOR.13.2.0.r1-15000-KAILUA.0
    Change-Id: I0ef9917f7d3e954b181dca442467d931cfa48f79
    Signed-off-by: Lunark :3 <jvillaltamatos3007@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -100,8 +100,6 @@ blob_fixups: blob_fixups_user_type = {
        'vendor/etc/media_codecs_kalama_vendor_without_dvenc.xml',
     ): blob_fixup()
         .regex_replace('.+media_codecs_(google_audio|google_c2|google_telephony|vendor_audio).+\n', ''),
-    'vendor/etc/ueventd.rc' : blob_fixup()
-        .add_line_if_missing('\n# Charger\n/sys/class/qcom-battery     night_charging            0660    system  system'),
     (
         'vendor/bin/poweropt-service',
         'vendor/lib64/libaodoptfeature.so',
